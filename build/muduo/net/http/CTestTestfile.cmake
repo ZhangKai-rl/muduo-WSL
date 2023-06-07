67799e290339e9f5294ef8fb7e0e903f194d2cb6
ZhangKai-rl <1521091656@qq.com>
2023-06-07 13:46:13 +0800

    忽略build

--- build/muduo/net/http/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zk/package/muduo-master/muduo/net/http
-# Build directory: /home/zk/package/muduo-master/build/muduo/net/http
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/muduo/net/inspect/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zk/package/muduo-master/muduo/net/inspect
-# Build directory: /home/zk/package/muduo-master/build/muduo/net/inspect
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
